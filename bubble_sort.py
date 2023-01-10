f53636ba823cd01b2199a04b776a40468601e7d5
Akeem <akeem.mighty@gmail.com>
2023-01-09 21:28:32 -0500

    Completed Bubble Sort Functionality + Tested Bubble Sort Function

--- bubble_sort.py
+++ bubble_sort.py
@@ -1,2 +1,16 @@
 def bubble_sort(array):
-    pass
+    index = len(array)
+    while index >= 0:
+        for j in range(index -1):
+            #if current element is greater than next element swap elements
+            if array[j] > array[j+1]:
+                array[j], array[j+1] = array[j+1], array[j]
+        index -= 1
+    return array
+
+#test sort
+
+sample_arr = [5, 8, 1, 6, 7, 2, 4, 3]
+print("Test input")
+print(*sample_arr, sep=" , ")
+print(bubble_sort(sample_arr))
