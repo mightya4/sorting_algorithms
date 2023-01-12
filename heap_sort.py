d19e5ba403e195677841c9fbeb527e6a2149a21a
Akeem <akeem.mighty@gmail.com>
2023-01-11 22:05:24 -0500

    Completed max_heapify Functionality

--- heap_sort.py
+++ heap_sort.py
@@ -1,2 +1,11 @@
 def max_heapify(A, heap_size, i):
-    pass
+    left = 2 * i + 1
+    right = 2 * i + 2
+    largest = i
+    if left < heap_size and A[left] > A[largest]:
+        largest = left
+    if right < heap_size and A[right] > A[largest]:
+        largest = right
+    if largest != i:
+        A[i], A[largest] = A[largest], A[i]
+        max_heapify(A, heap_size, largest)
