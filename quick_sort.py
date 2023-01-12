f701076eaa0ab935a0d41515468785a1fb8605af
Akeem <akeem.mighty@gmail.com>
2023-01-11 21:55:29 -0500

    Completed Quick Sort Functionality

--- quick_sort.py
+++ quick_sort.py
@@ -1,2 +1,23 @@
 def quick_sort(arr):
-    pass
+    less = []
+    pivotList = []
+    more = []
+
+    if len(arr) <= 1:
+        return arr
+    else:
+        pivot = arr[0]
+        #loop through each element and compare current element with pivot
+        for i in arr:
+            if i < pivot:
+                less.append(i)
+            elif i > pivot:
+                more.append(i)
+            else:
+                pivotList.append(i)
+        #create subproblems until base case is reached
+        less = quick_sort(less)
+        more = quick_sort(more)
+        #join lower, middle, upper array
+        return less + pivotList + more
+
